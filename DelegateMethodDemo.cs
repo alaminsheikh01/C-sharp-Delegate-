9f7cf2d0c8b04691537b45692dc8f196590628ea
alamin <alamin@gmail.com>
2020-12-19 11:55:11 +0600

    C sharp delegate

---
+++ DelegateMethodDemo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateDemo
+{
+    public delegate void showmsgdelegate(string message);
+    class DelegateMethodDemo
+    {
+        public void showMessage1(string msg)
+        {
+            System.Windows.MessageBox.Show("THIS IS METHOD 1: " + Environment.NewLine + msg);
+        }
+
+        public void showMessage2(string msg)
+        {
+            System.Windows.MessageBox.Show("THIS IS METHOD 2: " + Environment.NewLine + msg);
+        }
+        /// <summary>
+        /// this method returns a method hiddden behind the delegate showmsgdelegate
+        /// </summary>
+        /// <param name="methodtype">1 for showMessage1 and 2 for showMessage2</param>
+        /// <returns></returns>
+        public showmsgdelegate getMethod(string methodtype)
+        {
+            if(methodtype.Equals("1"))
+            {
+                return this.showMessage1;
+            }
+            else if(methodtype.Equals("2"))
+            {
+                return this.showMessage2;
+            }
+            else
+            {
+                throw new Exception("Invalid method type passed.");
+            }
+        }
+    }
+}
